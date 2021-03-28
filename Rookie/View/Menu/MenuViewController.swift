11ee41746befb176c7fe08cd0ae7f080ee58ba84
yoo-kie <alice0926@hanmail.net>
2021-03-29 00:48:04 +0900

    ✍️ Menu화면 CollectionView 의존성 분리
    - RookiesUICollectionViewDataSource 생성

--- Rookie/View/Menu/MenuViewController.swift
+++ Rookie/View/Menu/MenuViewController.swift
@@ -15,9 +15,9 @@ final class MenuViewController: UIViewController {
     @IBOutlet var rookiesCollectionView: UICollectionView!
     
     var menuDate: String = ""
-    private var rookies: [Rookie] = []
     
     private let menuModel = MenuModel()
+    private var rookiesUICollectionViewDataSource = RookiesUICollectionViewDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +27,7 @@ final class MenuViewController: UIViewController {
         
         menuModel.fetchRookies(with: menuDate) { [weak self] rookies in
             guard let self = self else { return }
-            self.rookies = rookies
+            self.rookiesUICollectionViewDataSource.rookies = rookies
         }
     }
 
@@ -35,50 +35,19 @@ final class MenuViewController: UIViewController {
         rookiesCollectionView.layer.cornerRadius = Constant.Layer.cornerRadius
         
         let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        
         if let version = version {
             versionLabel.text = version
         }
     }
     
     func configureColletionView() {
-        rookiesCollectionView.delegate = self
-        rookiesCollectionView.dataSource = self
+        rookiesCollectionView.delegate = rookiesUICollectionViewDataSource
+        rookiesCollectionView.dataSource = rookiesUICollectionViewDataSource
         rookiesCollectionView.register(
             UINib(nibName: "RookiesCollectionViewCell", bundle: nil),
             forCellWithReuseIdentifier: "rookiesCell"
         )
-    }
-    
-}
-
-extension MenuViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-    
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath
-    ) -> CGSize {
-        return CGSize(width: 30, height: 30)
-    }
-    
-    func collectionView(
-        _ collectionView: UICollectionView,
-        numberOfItemsInSection section: Int
-    ) -> Int {
-        return self.rookies.count
-    }
-    
-    func collectionView(
-        _ collectionView: UICollectionView,
-        cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "rookiesCell", for: indexPath) as? RookiesCollectionViewCell
-        else {
-            return collectionView.dequeueReusableCell(withReuseIdentifier: "rookiesCell", for: indexPath)
-        }
-            
-        cell.bind(rookies: rookies, indexPath: indexPath)
-        
-        return cell
     }
     
 }
--- Rookie/View/Menu/RookiesUICollectionViewDataSource.swift
+++ Rookie/View/Menu/RookiesUICollectionViewDataSource.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2021 yookie. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RookiesUICollectionViewDataSource: NSObject, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    var rookies: [Rookie] = []
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width: 30, height: 30)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return self.rookies.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "rookiesCell", for: indexPath) as? RookiesCollectionViewCell
+        else {
+            return collectionView.dequeueReusableCell(withReuseIdentifier: "rookiesCell", for: indexPath)
+        }
+            
+        cell.bind(rookies: rookies, indexPath: indexPath)
+        
+        return cell
+    }
+    
+}
